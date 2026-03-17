523da7daf7d0237c514954daec7491a919f0d520
K SHRI LALITHA <shreelalithar@gmail.com>
2026-03-17 21:16:17 +0530

    Create length-of-last_word.c
    Given a string s consisting of words and spaces, return the length of the last word in the string.

---
+++ Strings/length-of-last_word.c
@@ -0,0 +1,18 @@
+// Problem: Length of Last Word
+// Platform: LeetCode
+// Language: C
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+int lengthOfLastWord(char* s) {
+    int count=0;
+    int n=strlen(s);
+    int i=n-1;
+      while(i>=0 && s[i]==' '){
+          i--;
+        }
+    while(i>=0 && s[i]!=' '){
+          count+=1;
+          i--;
+        }
+    return count;
+}
