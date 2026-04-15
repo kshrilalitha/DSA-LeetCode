3696fa707a61e000cee1dbe615cd0864d395f832
K SHRI LALITHA <shreelalithar@gmail.com>
2026-04-15 23:16:14 +0530

    Create search-insert-position.c
    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.

---
+++ Arrays/search-insert-position.c
@@ -0,0 +1,13 @@
+// Problem: Search Insert Position
+// platform: LeetCode
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
+int searchInsert(int* nums, int numsSize, int target) {
+    for(int i=0;i<numsSize;i++){
+         if(nums[i]>=target){
+            return i;
+         }
+    } 
+   return numsSize;
+}
